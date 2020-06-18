68f88a4bfb504465b8ab1e8370ac4e133c17f59e
Tayna Paulo <55710685+taynamoreira@users.noreply.github.com>
2020-06-18 18:53:10 -0300

    Update formulario.js

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -6,27 +6,27 @@ function validar() {
     var telefone = document.getElementById("telefone")
     var mensagem = document.getElementById("mensagem")
 
-    if (nome.value == "") {
+    if (nome.value.length == 0) {
         alert('Nome não informado!')
         nome.focus();
         return;
     }
-    if (sobrenome.value == "") {
+    if (sobrenome.value.length == 0) {
         alert('Sobrenome não informado!');
         sobrenome.focus();
         return;
     }
-    if (email.value == "") {
+    if (email.value.length == 0) {
         alert('Email não informado!');
         email.focus();
         return;
     }
-    if (telefone.value == "") {
+    if (telefone.value.length == 0) {
         alert('Telefone não informado!');
         telefone.focus();
         return;
     }
-    if (mensagem.value == "") {
+    if (mensagem.valu.length == 0) {
         alert('Digite sua mensagem!');
         mensagem.focus();
         return;
