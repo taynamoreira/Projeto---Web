8a27c1b053cf26a40cb8bf4346ca7b72bb065bea
Tayna Paulo <55710685+taynamoreira@users.noreply.github.com>
2020-06-18 09:23:56 -0300

    Add files via upload

---
+++ javascript/formulario.js
@@ -0,0 +1,36 @@
+function validar() {
+
+    var nome = document.getElementById("nome")
+    var sobrenome = document.getElementById("sobrenome")
+    var email = document.getElementById("email")
+    var telefone = document.getElementById("telefone")
+    var mensagem = document.getElementById("mensagem")
+
+    if (nome.value == "") {
+        form.innerHTML = 'Nome não informado!'
+        nome.focus();
+        return;
+    }
+    if (sobrenome.value == "") {
+        alert('Sobrenome não informado!');
+        sobrenome.focus();
+        return;
+    }
+    if (email.value == "") {
+        alert('Email não informado!');
+        email.focus();
+        return;
+    }
+    if (telefone.value == "") {
+        alert('Telefone não informado!');
+        telefone.focus();
+        return;
+    }
+    if (mensagem.value == "") {
+        alert('Digite sua mensagem!');
+        mensagem.focus();
+        return;
+    }
+    alert('Mensagem enviada!');
+}
+
