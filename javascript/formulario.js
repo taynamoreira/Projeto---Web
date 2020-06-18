3a23453b6b8e1923a05d5b022a88ddf93fad8d41
Tayna Paulo <55710685+taynamoreira@users.noreply.github.com>
2020-06-18 18:59:07 -0300

    Update formulario.js

--- javascript/formulario.js
+++ javascript/formulario.js
@@ -7,29 +7,20 @@ function validar() {
     var mensagem = document.getElementById("mensagem")
 
     if (nome.value.length == 0) {
-        alert('Nome não informado!')
-        nome.focus();
-        return;
+        alert('Nome não informado!');
     }
     if (sobrenome.value.length == 0) {
         alert('Sobrenome não informado!');
-        sobrenome.focus();
-        return;
     }
     if (email.value.length == 0) {
         alert('Email não informado!');
-        email.focus();
-        return;
     }
     if (telefone.value.length == 0) {
         alert('Telefone não informado!');
-        telefone.focus();
-        return;
     }
     if (mensagem.valu.length == 0) {
         alert('Digite sua mensagem!');
-        mensagem.focus();
-        return;
+
     } else {
         alert('Mensagem enviada!');
     }
