772c3f0a14e73b3fb8b949bb60e48415b0aebe6b
Tayna Paulo <55710685+taynamoreira@users.noreply.github.com>
2020-06-18 21:25:10 -0300

    Add files via upload

---
+++ javascript/bemvindo.js
@@ -0,0 +1,4 @@
+function bemvindo() {
+    var nome =  window.prompt(`Olá! Qual é o seu nome?`)
+    alert(`Olá, ${nome}. Caso tenha alguma dúvida mande-nos uma mensagem através do "Fale Conosco".`)
+}
